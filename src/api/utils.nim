24620c0fce2616eda2a2970889d84f5205665d5c
Lyndon <l.leong1618@gmail.com>
2023-02-11 19:12:01 +0000

    fix time bug mm -> MM

--- src/api/utils.nim
+++ src/api/utils.nim
@@ -1,18 +1,11 @@
 import 
     unittest,
-    strformat,
     strutils,
-    uri,
     options,
-    algorithm,
     times
 
 import 
-    ../models/models,
     jsony
-
-
-import jsony, options
 
 proc parseHook*[T](s: string, i: var int, v: var Option[seq[T]]) =
     eatSpace(s, i)
@@ -34,17 +27,17 @@ proc parseHook*[T](s: string, i: var int, v: var Option[seq[T]]) =
 
 
 proc dumpHook*(s: var string, v: Option[DateTime]) =
-    s.add("\"" & v.get().format("yyyy-mm-dd'T'hh:mm:ss'.'fff'Z'") & "\"" )
+    s.add("\"" & v.get().format("yyyy-MM-dd'T'hh:mm:ss'.'fff'Z'") & "\"" )
 
 proc dumpHook*(s: var string, v: DateTime) =
-    s.add("\"" & v.format("yyyy-mm-dd'T'hh:mm:ss'.'fff'Z'") & "\"" )
+    s.add("\"" & v.format("yyyy-MM-dd'T'hh:mm:ss'.'fff'Z'") & "\"" )
 
 proc parseHook*(s: string, i: var int, v: var DateTime) =
     ## jsony time convert
     ## runs through times and tries to parse them
     var str: string
     parseHook(s, i, str)
-    var timeFormats = @["yyyy-MM-dd", "yyyy-MM-dd hh:mm:ss", "yyyy-mm-dd'T'hh:mm:ss'.'fff'Z'"]
+    var timeFormats = @["yyyy-MM-dd", "yyyy-MM-dd hh:mm:ss", "yyyy-MM-dd'T'hh:mm:ss'.'fff'Z'"]
     for fmt in timeFormats:
         try:
             v = parse(str, fmt)
@@ -105,108 +98,22 @@ proc renameHook*(v: object, fieldName: var string) =
             return
 
 
-proc toKebab(s: string): string =
-    ## myapi  -> myapi
-    ## myApi  -> my-api
-    ## MyApi  -> my-api
-    ## MyAPI  -> my-api
-    ## my_api -> my-api
-    ## my_Api -> my-api
-    ## my_API -> my-api
-    result = s
-    var insertDash = true
-    var i = 0
-    while i <= result.high:
-        if i == 0:
-            result[i] = result[i].toLowerAscii()
-
-        if result[i] == '_':
-            result[i] = '-'
-            insertDash = false
-            i.inc()
-        if result[i] notin {'A'..'Z'}:
-            insertDash = true
-        if result[i] in {'A'..'Z'}:
-            result[i] = result[i].toLowerAscii()
-            if insertDash:
-                result.insert("-", i)
-                insertDash = false
-                i.inc()
-        i.inc()
-
-   
-    
-type
-    LetterCase = enum
-        None = "None",
-        PascalCase = "PascalCase",
-        CamelCase = "camelCase",
-        SnakeCase = "snake_case",
-        KebabCase = "kebab-case"
-
-
-proc amzUrlEncodeObject[T](obj: T, letterCase: LetterCase = PascalCase): string =   
-    #  Encodes an object into a url encoded string.
-    #  ListMultipartUploadsRequest
-    #    bucket: "mybucket"
-    #    delimiter: "/"
-    #    encoding-type: "url"
-    #    key-marker: "mykey"
-    #    max-uploads: 1000
-    #    prefix: "myprefix"
-    #    upload-id-marker: "myuploadid"
-    #  becomes
-    #    bucket=mybucket&delimiter=%2F&encoding-type=url&key-marker=mykey&max-uploads=1000&prefix=myprefix&upload-id-marker=myuploadid
-
-    var keyVals: seq[(string, string)] = @[]
-
-    # convert to snake case my_Var to my_var
-    # convert pascal myVar to my-var
-    # 
-    for key, val in obj.fieldPairs():
-        var amzKey = case letterCase:
-            # of PascalCase: key.toPascal()
-            # of CamelCase: key.toCamel()
-            # of SnakeCase: key.toSnake()
-            of KebabCase: key.toKebab()
-            else: key
-
-        when val is Option:
-            if val.isSome():
-                keyVals.add((amzKey, $(val.get())))
-        else:
-            keyVals.add((amzKey, $val))
-    
-    keyVals.sort()
-
-    return encodeQuery(keyVals)
-
-
 suite "utility functions":
-    test "toKebab":
+
+    test "check amazon time format time":
+        let time = parse("2023-02-09T08:24:35.000Z", initTimeFormat "yyyy-MM-dd\'T\'HH:mm:ss\'.\'fff\'Z\'")
+        let expectedTime = fromUnix(1675931075).utc() # 2023-02-09T08:24:35.000Z
         check:
-            "myapi".toKebab() == "myapi"
-            "myApi".toKebab() == "my-api"
-            "MyApi".toKebab() == "my-api"
-            "MyAPI".toKebab() == "my-api"
-            "my_api".toKebab() == "my-api"
-            "my_Api".toKebab() == "my-api"
-            "my_API".toKebab() == "my-api"
-
-
-    test "encodeUrl-kebab":
-        let listMultipartUploadsRequest = ListMultipartUploadsRequest(
-            bucket: "mybucket",
-            delimiter: some("/"),
-            encodingType: some("url"),
-            keyMarker: some("mykey"),
-            maxUploads: some(1000),
-            prefix: some("myprefix"),
-            uploadIdMarker: some("myuploadid"),
-            expectedBucketOwner: some("mybucketowner"),
-        )
-        let expectedKebab = "bucket=mybucket&delimiter=%2F&encoding-type=url&expected-bucket-owner=mybucketowner&key-marker=mykey&max-uploads=1000&prefix=myprefix&upload-id-marker=myuploadid"
-        check listMultipartUploadsRequest.amzUrlEncodeObject(KebabCase) == expectedKebab
+            time == expectedTime
+    test "jsony time convert":
+        type
+            MyTimeObject = object
+                time: DateTime
+        let json = """{"time":"2023-02-09T08:24:35.000Z"}"""
+        let myTimeObject = json.fromJson(MyTimeObject)
+        let expectedTime = fromUnix(1675931075).utc() # 2023-02-09T08:24:35.000Z
+        check:
+            myTimeObject.time == expectedTime
 
     test "jsony loose frist char":
         type
@@ -224,9 +131,6 @@ suite "utility functions":
         let expectedObject = MyObject(id: "someId", myFancyField: "foo")
         check:
             myObject == expectedObject
-
-    test "parse time":
-        let expectedTime = parse("2023-02-09T08:24:35.000Z", initTimeFormat "yyyy-MM-dd\'T\'HH:mm:ss\'.\'fff\'Z\'")
 
     test "jsony loose object/arr":
         type

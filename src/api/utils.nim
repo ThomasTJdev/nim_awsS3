adad75f089c3878970809c4d6e204c9db672958b
Lyndon <l.leong1618@gmail.com>
2023-02-22 08:10:01 +0000

    add time formats

--- src/api/utils.nim
+++ src/api/utils.nim
@@ -30,20 +30,32 @@ proc dumpHook*(s: var string, v: Option[DateTime]) =
   if v.isNone:
     s.add("null")
   else:
-    s.add("\"" & v.get().format("yyyy-MM-dd'T'hh:mm:ss'.'fff'Z'") & "\"" )
+    s.add("\"" & v.get().format("yyyy-MM-dd'T'hh:mm:ss'.'fffzzz") & "\"" )
 
 proc dumpHook*(s: var string, v: DateTime) =
-    s.add("\"" & v.format("yyyy-MM-dd'T'hh:mm:ss'.'fff'Z'") & "\"" )
+    s.add("\"" & v.format("yyyy-MM-dd'T'hh:mm:ss'.'fffzzz") & "\"" )
 
 proc parseHook*(s: string, i: var int, v: var DateTime) =
     ## jsony time convert
     ## runs through times and tries to parse them
     var str: string
     parseHook(s, i, str)
-    var timeFormats = @["yyyy-MM-dd", "yyyy-MM-dd hh:mm:ss", "yyyy-MM-dd'T'hh:mm:ss'.'fff'Z'"]
+    var timeFormats = @[
+        "yyyy-MM-dd", 
+        "yyyy-MM-dd hh:mm:ss", 
+        "yyyy-MM-dd hh:mm:ssz", 
+        "yyyy-MM-dd hh:mm:sszz",
+        "yyyy-MM-dd hh:mm:sszzzz", 
+        "yyyy-MM-dd hh:mm:ss.fff",
+        "yyyy-MM-dd'T'hh:mm:ss'.'fffz", 
+        "yyyy-MM-dd'T'hh:mm:ss'.'fffzz", 
+        "yyyy-MM-dd'T'hh:mm:ss'.'fffzzz",
+        "yyyy-MM-dd'T'hh:mm:ss'.'fff'Z'", 
+        "yyyy-MM-dd'T'hh:mm:ss'.'fff'Z'zzz",
+        ]
     for fmt in timeFormats:
         try:
-            v = parse(str, fmt).utc()
+            v = parse(str, fmt, utc())
             return
         except:
             continue
@@ -102,20 +114,35 @@ proc renameHook*(v: object, fieldName: var string) =
 
 suite "utility functions":
     test "check amazon time format time":
-        let time = parse("2023-02-09T08:24:35.000Z", "yyyy-MM-dd\'T\'HH:mm:ss\'.\'fff\'Z\'").utc()
+        let time = parse("2023-02-09T08:24:35.000Z", "yyyy-MM-dd\'T\'HH:mm:ss\'.\'fffzzz", utc())
         let expectedTime = fromUnix(1675931075).utc() # 2023-02-09T08:24:35.000Z
         check:
             time == expectedTime
-    test "jsony time convert":
+    test "jsony time convert - parse":
         type
             MyTimeObject = object
                 time: DateTime
-        let json = """{"time":"2023-02-09T08:24:35.000Z"}"""
-        let myTimeObject = json.fromJson(MyTimeObject)
-        let expectedTime = fromUnix(1675931075).utc() # 2023-02-09T08:24:35.000Z
+        let json = """[
+            {"time":"2023-02-09T08:24:35.000Z"},
+            {"time":"2023-02-09T08:24:35.000Z+00:00"},
+            {"time":"2023-02-09T08:24:35.000Z+01:00"},
+        ]"""
+        let timesArr = json.fromJson(seq[MyTimeObject])
         check:
-            myTimeObject.time == expectedTime
+            timesArr[0].time == dateTime(2023, mFeb, 9, 8, 24, 35, 0, utc()) # 2023-02-09T08:24:35.000Z
+            timesArr[1].time == dateTime(2023, mFeb, 9, 8, 24, 35, 0, utc()) # 2023-02-09T08:24:35.000Z
+            timesArr[2].time == dateTime(2023, mFeb, 9, 7, 24, 35, 0, utc()) # 2023-02-09T07:24:35.000Z
 
+    test "jsony time convert - dump":
+        type
+            MyTimeObject = object
+                time: DateTime
+        let time = dateTime(2023, mFeb, 9, 8, 24, 35, 0, utc()) # 2023-02-09T08:24:35.000Z
+        let myTimeObject = MyTimeObject(time: time)
+        let json = myTimeObject.toJson()
+        check:
+            json == """{"time":"2023-02-09T08:24:35.000Z"}"""
+            
     test "jsony loose frist char":
         type
             MyObject = object
@@ -149,3 +176,6 @@ suite "utility functions":
             d1.fromJson(MyType) == MyType(cat: some(@[Cat(name: "sparky")]))
             d2.fromJson(MyType) == MyType(cat: some(@[Cat(name: "sparky")]))
             d3.fromJson(MyType) == MyType()
+
+
+            
